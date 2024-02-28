a739e982db2c28dec2e46238dae9e372a0472bf7
Sergio Chiquete Castro <sergiogccastro@gmail.com>
2024-02-27 18:45:32 -0800

    Create ReverseANumber.cpp

---
+++ ReverseANumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, r;
+
+	cout <<"Enter n: ";
+	cin >> n;
+	
+
+	while (n > 0) {
+
+		r = n % 10;
+		n = n / 10;
+		cout << r << endl;
+	}
+
+
+
+}
